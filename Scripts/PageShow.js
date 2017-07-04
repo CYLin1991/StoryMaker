2065fbe213569037289f35cb61f0aaece1d56fe2
CYLin1991 <marioreda3@gmail.com>
2017-07-04 15:46:29 +0800

    Add files via upload

---
+++ Scripts/PageShow.js
@@ -0,0 +1,96 @@
+﻿$(document).ready(function () {
+    if ($('.item').length > 0) {
+        $('#divNone').hide();
+        $('.item').first().addClass('active');
+    }
+    else
+    {
+        $('#divRec').hide();
+        $('#carousel1').hide();
+    } 
+    $('.btnA').click(function (even) {
+        var storyT = $('#lblStory').text();
+        var pageT = $(this).children().text().toString();
+        var imageT = $(this).attr('id').toString();
+        var image2 = $(this).parent(".imgContain");
+        $(image2).animate({ width: '0px', height: '0px' }, 300, 'linear', function () {
+            $(this).hide();
+            $(document).delay(5000, function () {
+                $.ajax({
+                    type: 'POST',
+                    url: 'StoryService.asmx/DelImage',
+                    data: { Image: imageT, Story: storyT, Page: pageT },
+                    success: function () {
+                        alert('您刪除了一張圖片');
+
+                        window.location.href = "PageShow.aspx";
+                    },
+                    error: function () {
+                        alert('操作無效');
+                    }
+                });
+            });
+
+        });
+        //alert(storyT + " ; " + pageT + " ; " + imageT);
+
+    });
+
+
+    //var embed1 = document.createElement("embed");
+    //	        embed1.src = "0001.wav"; //你的音效檔案位置 
+    //	        embed1.hidden = "true";
+    //	        document.body.appendChild(embed1); 
+    var pageNo = 1;
+    var MusicNo;
+    var Sound;
+
+    function add0(str, length) {
+        if (str.length >= length)
+            return str
+        else
+            return ("0" + str)
+    }
+
+    function MusicChange()
+    {       
+        MusicNo = "#sound" + add0(pageNo.toString(), 2);
+        Sound = $(MusicNo);
+        if (Sound.length > 0)//檢查ID為sound"NN"(<audio>)的物件是否存在
+            Sound.attr('controls', 'controls');
+    }
+    MusicChange();
+    //alert(MusicNo);
+    function MusicStop()
+    {       
+        if (Sound.length > 0) {
+            Sound.trigger('pause');
+            Sound.prop("currentTime", 0);
+            Sound.removeAttr("controls");
+        }
+    }
+
+    $('.carousel-control').click(function () {
+        MusicStop();
+        switch ($(this).prop('id'))
+        {
+            case "prev":
+                if (pageNo > 1)
+                    pageNo--;
+                else pageNo = $('.item').length;
+                break;
+            case "next":
+                if (pageNo < $('.item').length)
+                    pageNo++;
+                else pageNo = 1;
+                break;
+        }
+        MusicChange();
+        //alert(MusicNo);
+    })
+
+    //$('input.imgBtn').mouseover(function () {
+    //    $(this).attr("bacrground-color","gold")
+    //})
+})
+
---
+++ Scripts/record01.js
@@ -0,0 +1,104 @@
+﻿if (!Date.prototype.toISOString) {
+
+    (function () {
+
+        function pad(number) {
+            var r = String(number);
+            if (r.length === 1) {
+                r = '0' + r;
+            }
+            return r;
+        }
+
+        Date.prototype.toISOString = function () {
+            return this.getUTCFullYear()
+                + '-' + pad(this.getUTCMonth() + 1)
+                + '-' + pad(this.getUTCDate())
+                + 'T' + pad(this.getUTCHours())
+                + ':' + pad(this.getUTCMinutes())
+                + ':' + pad(this.getUTCSeconds())
+                + '.' + String((this.getUTCMilliseconds() / 1000).toFixed(3)).slice(2, 5)
+                + 'Z';
+        };
+
+    }());
+}
+
+window.onload = function () {
+
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
+    var recorder;
+
+    var d = document.getElementById('d');
+    for (var i = 0; i < 256; i++) {
+        d.innerHTML += '<div></div>';
+    }
+    var dd = document.querySelectorAll('#d div');
+
+    var s = document.getElementById('s');
+    var p = document.getElementById('p');
+    var timer;
+    var context = new AudioContext();
+
+    navigator.getUserMedia({ audio: true }, function (stream) {
+        var microphone = context.createMediaStreamSource(stream);
+        var analyser = context.createAnalyser();
+        microphone.connect(analyser);
+        //analyser.connect(context.destination);
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(analyser.fftSize);
+        analyser.getByteFrequencyData(dataArray);
+
+        function update() {
+            console.log(dataArray);
+            analyser.getByteFrequencyData(dataArray);
+            for (var j = 0; j < 256; j++) {
+                dd[j].style.height = dataArray[j] + 'px';
+                dd[j].style.background = 'rgba(' + (255 - j) + ',' + j * 2 + ',0,1)';
+            }
+            timer = setTimeout(update, 20);
+        }
+
+        s.onclick = function () {
+            clearTimeout(timer);
+            p.disabled = false;
+            s.disabled = true;
+            recorder.stop();
+            createDownloadLink();
+            recorder.clear();
+        };
+
+        p.onclick = function () {
+            recorder = new Recorder(microphone);
+            recorder.record();
+            p.disabled = true;
+            s.disabled = false;
+            update();
+        };
+
+        function createDownloadLink() {
+            recorder.exportWAV(function (blob) {
+                var url = URL.createObjectURL(blob);
+                var li = document.createElement('li');
+                var au = document.createElement('audio');
+                var hf = document.createElement('a');
+
+                au.controls = true;
+                au.src = url;
+                hf.href = url;
+                hf.download = new Date().toISOString() + '.wav';
+
+                hf.innerHTML = hf.download;
+                li.appendChild(au);
+                li.appendChild(hf);
+                recordingslist.appendChild(li);
+            });
+        }
+
+    }, function () {
+        console.log('error');
+    });
+};
